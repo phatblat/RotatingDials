c91023e2ad1477df0e1aee9e03afa7f46e5e9774
Ben Chatelain <benchatelain@gmail.com>
2015-06-19 16:55:41 -0600

    Add three circles

--- RotatingDials/CircleViewController.swift
+++ RotatingDials/CircleViewController.swift
@@ -11,11 +11,22 @@ import UIKit
 class CircleViewController: UIViewController {
 
     @IBOutlet weak var orangeCircle: UIView!
+    @IBOutlet weak var greenCircle: UIView!
+    @IBOutlet weak var blueCircle: UIView!
+
+    let diameter: CGFloat = 250
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        orangeCircle.layer.cornerRadius = view.bounds.width / 3
+        view.autoresizesSubviews = false
+
+        view.setTranslatesAutoresizingMaskIntoConstraints(true)
+        view.bounds = CGRect(x: 0, y: 0, width: diameter * 2, height: diameter * 2)
+
+        orangeCircle.layer.cornerRadius = diameter / 2
+        greenCircle.layer.cornerRadius = diameter / 2
+        blueCircle.layer.cornerRadius = diameter / 2
     }
 
 }
